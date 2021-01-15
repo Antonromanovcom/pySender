2e77dcc9fefd2a5040a8b4a581cc87bf9d7b0855
Latinist <ABRomanov@sberbank.ru>
2021-01-15 14:07:21 +0300

    Прикрутили первый GET

--- hw.py
+++ hw.py
@@ -1,1 +1,8 @@
-print("HW!")
+# Импорт библиотеки requests
+import requests
+# Запрос GET (Отправка только URL без параметров)
+response = requests.get("http://api.open-notify.org/astros.json")
+# Вывод кода
+print(response.status_code)
+# Вывод ответа, полученного от сервера API
+print(response.json())
